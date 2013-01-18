ca8b55a593ae42783fa130ed30dde4d1ac9e8f4c
Hugh Cole-Baker <sigmaris@gmail.com>
2013-01-18 19:56:37 +0000

    Add some missing code

--- gssapi/creds.py
+++ gssapi/creds.py
@@ -1,16 +1,17 @@
 from __future__ import absolute_import
 
-from ctypes import cast, byref, c_char_p, c_void_p, string_at
+from ctypes import cast, byref, c_uint
 
 from .gssapi_h import (
-    GSS_C_NO_CREDENTIAL, GSS_C_NO_NAME, GSS_C_INDEFINITE, GSS_C_NO_OID_SET, GSS_C_BOTH,
-    GSS_S_COMPLETE,
-    OM_uint32, gss_cred_id_t,
-    gss_init_sec_context, gss_accept_sec_context, gss_delete_sec_context, gss_release_buffer,
-    gss_release_cred, gss_release_name
+    GSS_C_NO_NAME, GSS_C_INDEFINITE, GSS_C_NO_OID_SET, GSS_C_BOTH,
+    GSS_ERROR,
+    OM_uint32, gss_cred_id_t, gss_name_t, gss_OID_set, gss_cred_usage_t,
+    gss_acquire_cred,
+    gss_release_cred, gss_release_oid_set
 )
-from .error import GSSException, GSSMechException
-from .names import MechName
+from .error import GSSException
+from .names import Name
+from .oids import OIDSet
 
 
 class Credential(object):
@@ -19,3 +20,43 @@ class Credential(object):
     def __init__(self, desired_name=GSS_C_NO_NAME, time_req=GSS_C_INDEFINITE,
         desired_mechs=GSS_C_NO_OID_SET, cred_usage=GSS_C_BOTH):
         super(Credential, self).__init__()
+        self._cred = gss_cred_id_t()
+
+        minor_status = OM_uint32()
+
+        if isinstance(desired_name, Name):
+            desired_name = desired_name._name
+        else:
+            desired_name = cast(desired_name, gss_name_t)
+
+        if isinstance(desired_mechs, OIDSet):
+            desired_mechs = desired_mechs._oid_set
+        else:
+            desired_mechs = cast(desired_mechs, gss_OID_set)
+
+        actual_mechs = gss_OID_set()
+        time_rec = c_uint()
+
+        retval = gss_acquire_cred(
+            byref(minor_status),
+            desired_name,
+            time_req,
+            desired_mechs,
+            cred_usage,
+            byref(self._cred),
+            byref(actual_mechs),
+            byref(time_rec)
+        )
+        try:
+            if GSS_ERROR(retval):
+                raise GSSException(retval, minor_status)
+
+            self._mechs = OIDSet(actual_mechs)
+        except:
+            if self._cred:
+                gss_release_cred(byref(minor_status), byref(self._cred))
+            if actual_mechs:
+                gss_release_oid_set(byref(minor_status), byref(actual_mechs))
+            raise
+
+        # TODO: expiry time
--- gssapi/names.py
+++ gssapi/names.py
@@ -4,22 +4,31 @@ from ctypes import byref, cast, c_char_p, c_void_p
 
 from .gssapi_h import (
     GSS_C_NO_OID, GSS_C_NO_NAME, GSS_S_COMPLETE,
-    OM_uint32, gss_buffer_desc, gss_name_t,
+    OM_uint32, gss_buffer_desc, gss_name_t, gss_OID,
     gss_import_name, gss_display_name, gss_release_name, gss_release_buffer
 )
 from .error import GSSException, buf_to_str
+from .oids import OID
 
 
 class Name(object):
-    """Represents an internal GSSAPI name"""
+    """Represents an internal GSSAPI name (wraps a gss_name_t)"""
 
     def __init__(self, name, name_type=GSS_C_NO_OID):
         super(Name, self).__init__()
+
         minor_status = OM_uint32()
+
         name_buffer = gss_buffer_desc()
         name_buffer.length = len(name)
         name_buffer.value = cast(c_char_p(name), c_void_p)
+
         self._name = gss_name_t()
+        if isinstance(name_type, OID):
+            name_type = byref(name_type._oid)
+        else:
+            name_type = cast(name_type, gss_OID)
+
         retval = gss_import_name(
             byref(minor_status), byref(name_buffer), name_type, byref(self._name)
         )
@@ -42,10 +51,13 @@ class Name(object):
             gss_release_buffer(byref(minor_status), byref(out_buffer))
 
     def release(self):
-        if self._name:
+        if hasattr(self, '_name') and self._name:
             minor_status = OM_uint32()
             gss_release_name(byref(minor_status), byref(self._name))
             self._name = cast(GSS_C_NO_NAME, gss_name_t)
+
+    def canonicalize(self):
+        pass
 
     def __del__(self):
         self.release()
