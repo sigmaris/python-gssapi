c5607cd4c272d679fd48dd359a8d26a37a6ed95d
Hugh Cole-Baker <sigmaris@gmail.com>
2013-09-14 18:30:13 +0100

    Organise requirements and .h generated files
    Add a setup.py

--- gssapi/types_h.py
+++ gssapi/types_h.py
@@ -5,8 +5,8 @@ import platform
 _currentplatform = platform.system()
 
 if _currentplatform == 'Darwin':
-    from .mac.gssapi_h import *
+    from .mac.types_h import *
 elif _currentplatform == 'Linux':
-    from .linux.gssapi_h import *
+    from .linux.types_h import *
 else:
     raise NotImplemented("Support for {0} platform is not (yet) implemented".format(_currentplatform))
---
+++ setup.py
@@ -0,0 +1,22 @@
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup, find_packages
+
+setup(
+    name="python-gssapi",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests.*", "tests"]),
+
+    install_requires=[
+        'pyasn1==0.1.6',
+        'ctypesgen==0.r125'
+    ],
+
+    # metadata for upload to PyPI
+    author="Hugh Cole-Baker",
+    author_email="hugh@sigmaris.info",
+    description="An object-oriented interface to GSSAPI for Python",
+    license="BSD",
+    keywords="gssapi kerberos",
+    url="https://github.com/sigmaris/python-gssapi",
+)
