c5b4fb1912862e3c08e3a9166efb2f0c180f2420
Hugh Cole-Baker <sigmaris@gmail.com>
2013-09-25 10:49:37 +0100

    Bump version (still very much alpha)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-gssapi",
-    version="0.1.1",
+    version="0.2.0",
     packages=find_packages(exclude=["tests.*", "tests"]),
 
     install_requires=[
