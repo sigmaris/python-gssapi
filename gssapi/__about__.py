e98b647021b2c2dc3a2098e70da386059b437da7
Hugh Cole-Baker <sigmaris@gmail.com>
2014-07-18 18:12:16 +0100

    Begin development of version 0.6.4

--- gssapi/__about__.py
+++ gssapi/__about__.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 __title__ = 'python-gssapi'
 __author__ = 'Hugh Cole-Baker and contributors'
-__version__ = '0.6.3'
+__version__ = '0.6.4pre'
 
 __license__ = 'The MIT License (MIT)'
 __copyright__ = 'Copyright 2014 {0}'.format(__author__)
