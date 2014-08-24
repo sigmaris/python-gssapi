22a0e42906b1a27e04bbd6cab022a84ea9de1928
Hugh Cole-Baker <sigmaris@gmail.com>
2014-08-24 17:04:50 +0100

    Tests for import & export creeds
    and a few test fixes

--- tests/creds.py
+++ tests/creds.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 
+import platform
 import unittest
 
 from gssapi import (
-    Credential, NoCredential, CredentialsExpired, GSSCException,
-    S_NO_CRED, S_CREDENTIALS_EXPIRED, C_INITIATE, C_ACCEPT
+    bindings,
+    Credential, NoCredential, CredentialsExpired, GSSException, GSSCException,
+    S_NO_CRED, C_INITIATE, C_ACCEPT
 )
 
 
@@ -25,6 +27,12 @@ class DefaultInitCredentialTest(unittest.TestCase):
             self.cred.name
         except (NoCredential, CredentialsExpired):
             self.skipTest("No default init credential available, try running with a Kerberos ticket.")
+        self.is_heimdal_mac = False
+        if platform.system() == 'Darwin':
+            mac_ver = platform.mac_ver()[0].split('.')
+            if int(mac_ver[0]) >= 10 and int(mac_ver[1]) >= 7:
+                self.is_heimdal_mac = True
+
 
     def test_lifetime(self):
         first_lifetime = self.cred.lifetime
@@ -39,6 +47,35 @@ class DefaultInitCredentialTest(unittest.TestCase):
     def test_name(self):
         self.assertGreater(len(str(self.cred.name)), 0)
 
+    def test_export(self):
+        if self.is_heimdal_mac:
+            self.skipTest("gss_export_cred is bugged on Mac OS X 10.7+")
+        if not hasattr(bindings.C, 'gss_export_cred'):
+            self.skipTest("No support for gss_export_cred")
+        else:
+            self.assertGreater(len(str(self.cred.export())), 0)
+
+    def test_export_import(self):
+        if self.is_heimdal_mac:
+            self.skipTest("gss_export_cred is bugged on Mac OS X 10.7+")
+        if not hasattr(bindings.C, 'gss_export_cred'):
+            self.skipTest("No support for gss_export_cred")
+        else:
+            orig_name = self.cred.name
+            exported_token = self.cred.export()
+            imported_cred = Credential.imprt(exported_token)
+            self.assertEqual(orig_name, imported_cred.name)
+
+    def test_export_import_raises(self):
+        if self.is_heimdal_mac:
+            self.skipTest("gss_export_cred is bugged on Mac OS X 10.7+")
+        if not hasattr(bindings.C, 'gss_export_cred'):
+            self.skipTest("No support for gss_export_cred")
+        else:
+            exported_token = self.cred.export()
+            with self.assertRaises(GSSException):
+                # Cutting off characters should make token invalid
+                Credential.imprt(exported_token[4:])
 
 class DefaultAcceptCredentialTest(DefaultInitCredentialTest):
 
@@ -47,11 +84,19 @@ class DefaultAcceptCredentialTest(DefaultInitCredentialTest):
             self.cred = Credential(usage=C_ACCEPT)
             self.cred.name
         except GSSCException as exc:
-            if exc.maj_status == S_NO_CRED or 'Permission denied' in exc.message:
+            if (
+                exc.maj_status == S_NO_CRED or 'Permission denied' in exc.message
+                or 'keytab is nonexistent or empty' in exc.message
+            ):
                 self.skipTest("No default accept credential available, "
                     "try running with a Kerberos keytab readable.")
             else:
                 raise
+        self.is_heimdal_mac = False
+        if platform.system() == 'Darwin':
+            mac_ver = platform.mac_ver()[0].split('.')
+            if int(mac_ver[0]) >= 10 and int(mac_ver[1]) >= 7:
+                self.is_heimdal_mac = True
 
     def test_usage(self):
         self.assertEqual(C_ACCEPT, self.cred.usage)
